1de0ad0bd9e3d18be7baec431bc6645049c2789e
Administrator <1602311143@qq.com>
2023-08-16 14:42:37 +0800

    first1

--- Python流程控制语法IF.py
+++ Python流程控制语法IF.py
@@ -1,8 +1,7 @@
-'''
+"""
+if
+"""
+a=1
+if a<2 :
+    print("a is less than 2")
 
-'''
-b = 2
-# Set the value of c to the value of a if the value of a is greater than the value of b, otherwise set it to the value of b
-c = a if a > b else b
-# Print the value of c
-print(c)
